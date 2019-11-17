fd353b8b7a77be2edc0773294a67d84eb2616c93
gretchonian <gretchonian@hotmail.com>
2019-11-17 06:47:06 -0800

    contact cleanup

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_15_003849) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "contacts", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "phone_number"
-    t.text "project_details"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
