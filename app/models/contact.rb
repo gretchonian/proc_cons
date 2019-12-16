cf205d02c650863b3d21cdd5bad458555862cf21
gretchonian <gretchonian@hotmail.com>
2019-12-16 10:06:53 -0800

    validations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,5 @@
 class Contact < ApplicationRecord
   validates :company_name, presence: true, :on => :create
-  # validates_format_of :address, :with => /\A[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info))\z/i, :on => :create
   validates_format_of :phone_number, :with => /\A(\d{10}|\(?\d{3}\)?[-. ]\d{3}[-.]\d{4})\z/, :on => :create
   validates_format_of :email, :with => /\A[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info))\z/i, :on => :create
   validates_presence_of :project_details
